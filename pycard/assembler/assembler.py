629643de81157ac86685301069da332dadc22517
iivlx <iivlx@iivlx.com>
2022-11-10 15:57:07 -0800

    Update directory structure...

---
+++ pycard/assembler/assembler.py
@@ -0,0 +1,8 @@
+def showUsage():
+  print("Usage: assembler.py [INPUT] [OUTPUT]")
+    
+def main():
+  if len(sys.argv) < 1: return showUsage()
+
+if __name__ == '__main__':
+  main()
---
+++ pycard/pycard.py
@@ -0,0 +1,8 @@
+def showUsage():
+  print("Usage: pycard.py [DECK]")
+    
+def main():
+  if len(sys.argv) < 1: return showUsage()
+
+if __name__ == '__main__':
+  main()
