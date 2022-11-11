63855412f2d7628a83bbe4b6814a7f3bb5352198
iivlx <iivlx@iivlx.com>
2022-11-10 16:51:53 -0800

    Add outline to process first token of each line...

--- pycard/assembler/assembler.py
+++ pycard/assembler/assembler.py
@@ -3,16 +3,36 @@
 class Assembler:
   labels = []
   current_line = 1
+  current_address = 0
+  deck = []
 
   def __init__(self, lines):
     self.processLines(lines)
 
   def processLine(self, line):
-    for token in line.split():
-      if token.startswith(';'):
-        print(f"Comment on line {self.current_line}")
-        return
+    if line.isspace(): return
 
+    token = line.split(maxsplit=1)[0]
+    
+    if token.startswith(';'):
+      print(f"Comment on line {self.current_line}")
+      return
+    
+    elif token == ".org": pass # TODO: Implement .org directive.
+
+    elif token == "#define": pass # TODO: Implement define macro.
+    elif token == "#include": pass # TODO: Implement include macro.
+
+    elif token == "INP": pass
+    elif token == "CLA": pass
+    elif token == "ADD": pass
+    elif token == "TAC": pass
+    elif token == "SFT": pass
+    elif token == "OUT": pass
+    elif token == "STO": pass
+    elif token == "SUB": pass
+    elif token == "JMP": pass
+    elif token == "HRS": pass
     
   def processLines(self, lines):
     for line in lines:
@@ -22,3 +42,4 @@ class Assembler:
 if __name__ == '__main__':
   import main
   main.main()
+  
