62327bc8dc465cb24a024699e9ec3628a64d3eda
iivlx <iivlx@iivlx.com>
2022-11-10 19:22:19 -0800

    Generate program listing.

--- pycard/assembler/assembler.py
+++ pycard/assembler/assembler.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+CARDIAC_INSTRUCTIONS = ["INP", "CLA", "ADD", "TAC", "SFT", "OUT", "STO", "SUB", "JMP", "HRS"]
+
 def isAddress(data): return isData(data, 2)
 
 def isData(data, datalength=3):
@@ -7,11 +9,20 @@ def isData(data, datalength=3):
   
 def isLabel(data): return data.isalnum()
 
+def removeComments(line):
+  tokens = []
+  for token in line.split():
+    if ';' not in token:
+      tokens.append(token)
+    else:
+      break
+  return tokens
+
 class Assembler:
   labels = {}
+  comments = {}
   current_line = 1
   current_address = 0
-  increment_address = False
   deck = []
 
   preprocessLines = []
@@ -19,28 +30,10 @@ class Assembler:
   def __init__(self, lines):
     self.preprocess(lines)
 
-    for line in self.preprocessLines:
-      print(line)
-  
-  def cla(self, line):
-    """ CLA - Clear Load Address """
-    tokens = []
-    for token in line.split():
-      if ';' not in token:
-        tokens.append(token)
-      else:
-        break
-    
-    if len(tokens) > 2:
-      raise ValueError("Too many values.")
-    elif not isAddress(tokens[1]) and not isLabel(tokens[1]):
-      raise ValueError("Unexpected value.")
-
-    self.preprocessLines.append(f"{self.current_address:03}: CLA {tokens[1]}")
-    self.current_address += 1
+    self.generateListing()
 
   def data(self, line):
-    tokens = line.split()
+    tokens = removeComments(line)
     
     if len(tokens) < 2:
       raise ValueError("Expected value.")
@@ -50,18 +43,37 @@ class Assembler:
         raise ValueError("Unexpected value.")
 
     for token in tokens[1:]:
-      self.preprocessLines.append(f"{self.current_address:03}: {token}")
+      listline = ""
+      listline += f"{self.current_address:02}    {token}"
+      if self.current_address in self.labels:
+        listline+= f"    {self.labels[self.current_address]:<6}  "
+      else:
+        listline+= " "*12
+      listline += "DATA    "
+      listline += f"{tokens[1]}"
+
+      self.preprocessLines.append(listline)
       self.current_address += 1
+      
+
+  def generateListing(self):
+    last = 0
+    for line in self.preprocessLines:
+      if int(line[0:2]) - last > 1:
+        print()
+      last = int(line[0:2])
+
+      print(line)
 
   def label(self, line):
-    tokens = line.split()
+    tokens = removeComments(line)
 
     labelname = tokens[0].split(':')[0]
 
-    self.labels[labelname] = self.current_address
+    self.labels[self.current_address] = labelname
     
   def org(self, line):
-    tokens = line.split()
+    tokens = removeComments(line)
 
     if len(tokens) > 2:
       raise ValueError("Too many values.")
@@ -69,11 +81,15 @@ class Assembler:
       raise ValueError("Unexpected value.")
 
     self.current_address = int(tokens[1])
+  
+  def preprocess(self, lines):
+    self.preprocessLines = []
+    for line in lines:
+      self.preprocessLine(line)
+      self.current_line += 1
 
   def preprocessLine(self, line):
     if line.isspace(): return
-
-    self.increment_address = False
 
     token = line.split(maxsplit=1)[0]
     
@@ -87,24 +103,33 @@ class Assembler:
     elif token == "#define": pass # TODO: Implement define macro.
     elif token == "#include": pass # TODO: Implement include macro.
 
-    elif token == "INP": pass
-    elif token == "CLA": self.cla(line)
-    elif token == "ADD": pass
-    elif token == "TAC": pass
-    elif token == "SFT": pass
-    elif token == "OUT": pass
-    elif token == "STO": pass
-    elif token == "SUB": pass
-    elif token == "JMP": pass
-    elif token == "HRS": pass
-
-    #self.current_address += 1
+    elif token in CARDIAC_INSTRUCTIONS:
+      self.preprocessInstruction(line)
   
-  def preprocess(self, lines):
-    self.preprocessLines = []
-    for line in lines:
-      self.preprocessLine(line)
-      self.current_line += 1
+
+  def preprocessInstruction(self, line):
+    tokens = removeComments(line)
+    
+    if len(tokens) > 2:
+      raise ValueError("Too many values.")
+    elif not isAddress(tokens[1]) and not isLabel(tokens[1]):
+      raise ValueError("Unexpected value.")
+
+    listline = ""
+
+    listline += f"{self.current_address:02}    "
+    listline += f"{CARDIAC_INSTRUCTIONS.index(tokens[0])}"
+    listline += "xx"
+
+    if self.current_address in self.labels:
+      listline+= f"    {self.labels[self.current_address]:<6}  "
+    else:
+      listline+= " "*12
+
+    listline += f"{tokens[0]}    {tokens[1]} "
+    self.preprocessLines.append(listline)
+
+    self.current_address += 1
 
 if __name__ == '__main__':
   import main
